53f153118ca63f4b535ace6f6dceb55d6794ce45
Mattia Riboni <mattia@MacBook-Pro-di-Mattia.local>
2026-04-07 18:34:42 +0200

    Experiment 2

--- PCN/trainer.py
+++ PCN/trainer.py
@@ -1,9 +1,11 @@
 import torch
+import time
 
 def train_pcn_binary(model, data_loader, num_epochs, eta_infer, eta_learn,
 T_infer, T_learn, lambda_fact=2, device='mps'):
     model.to(device).train()
     print("training started")
+    start = time.time()
     for epoch in range(num_epochs):
         print(f"epoch: {epoch}")
         for x_batch, y_batch in data_loader:
@@ -39,3 +41,8 @@ T_infer, T_learn, lambda_fact=2, device='mps'):
                         weights[l] -= eta_learn * grad_Wl
                     grad_Wout = eps_sup.T @ inputs_latents[-1] / B
                     weights[-1] -= eta_learn * grad_Wout
+        end_epoch_time = time.time()
+        elapsed_time = end_epoch_time - start
+        avg_time_per_epoch = elapsed_time / (epoch + 1)
+        time_remaining = avg_time_per_epoch * (num_epochs - epoch - 1)
+        print(f"Time remaining: {time_remaining//60}m at {time.time()}")
